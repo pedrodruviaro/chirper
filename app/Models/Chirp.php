f41a261a5b0ca062d4283e914dbd35a953858abb
pedrodruviaro <pedrodruviaro@gmail.com>
2025-01-09 09:50:21 -0300

    feat: show chirp list and using the "format" method inside blade

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -3,10 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Chirp extends Model
 {
     protected $fillable = [
         'message'
     ];
+
+    /* User relation */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
